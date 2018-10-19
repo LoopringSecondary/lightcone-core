9d42eea33046832da6693605d7270a1a2880ef09
wangdong <wangdong@zhongan.io>
2018-10-20 00:40:27 +0800

    update

--- src/main/scala/org/loopring/lightcone/core/markets/OrderBook.scala
+++ src/main/scala/org/loopring/lightcone/core/markets/OrderBook.scala
@@ -16,9 +16,7 @@
 
 package org.loopring.lightcone.core
 
-import org.slf4j.LoggerFactory
-
-case class OrderBookInfo(
+case class OrderBookMetadata(
     numBuys: Int = 0, // number of visible buy orders
     numSells: Int = 0, // number of visible sell orders
     numHiddenBuys: Int = 0,
@@ -32,82 +30,15 @@ case class OrderBookInfo(
   def totalNumSells = numSells + numHiddenSells
 }
 
-case class MarketId(primaryToken: Address, secondaryToken: Address)
-
-case class OrderBookConfig(
-    maxNumBuys: Int,
-    maxNumSells: Int,
-    maxNumHiddenBuys: Int,
-    maxNumHiddenSells: Int
-)
-
 trait OrderBook[T] {
-  val marketId: MarketId
-  val config: OrderBookConfig
-
   def addOrder(order: Order[T]): Set[Ring[T]]
   def deleteOrder(orderId: ID): Set[RingID]
 
   def trigerMatch(): Set[Ring[T]]
 
   def getLastPrice(): Option[Rational]
-  def getOrderBookInfo(): OrderBookInfo
+  def getMetadata(): OrderBookMetadata
 
   def getTopBuys(num: Int, skip: Int = 0, includingHidden: Boolean = false): Seq[Order[T]]
   def getTopSells(num: Int, skip: Int = 0, includingHidden: Boolean = false): Seq[Order[T]]
-}
-
-abstract class OrderBookImpl[T](
-    val marketId: MarketId,
-    val config: OrderBookConfig
-)(
-    implicit
-    pendingRingPool: PendingRingPool[T]
-)
-  extends OrderBook[T] {
-
-  var orderMap = Map.empty[ID, Order[T]]
-
-  private val log = LoggerFactory.getLogger(getClass.getName)
-
-  def addOrder(order: Order[T]): Set[Ring[T]] = {
-    order.realActuals
-    null
-  }
-  def deleteOrder(orderId: ID): Set[RingID]
-
-  def trigerMatch(): Set[Ring[T]] = {
-    null
-  }
-
-  def getLastPrice(): Option[Rational]
-  def getOrderBookInfo(): OrderBookInfo
-
-  def getTopBuys(num: Int, skip: Int = 0, includingHidden: Boolean = false): Seq[Order[T]]
-  def getTopSells(num: Int, skip: Int = 0, includingHidden: Boolean = false): Seq[Order[T]]
-
-  implicit private class RichOrderInMarket[T](order: Order[T]) {
-    def isSell = order.tokenS == marketId.secondaryToken
-    def isBuy = !isSell
-    def price = if (isSell) order.rate else Rational(1) / order.rate
-
-    def realActuals() = {
-      val pendingAmountS = pendingRingPool.getOrderPendingAmountS(order.id)
-      if (pendingAmountS == 0) order.actuals
-      else {
-        val actuals = order.actuals
-        assert(actuals.amountS > 0)
-        val amountS = (actuals.amountS - pendingAmountS).max(0)
-        val r = Rational(amountS, actuals.amountS)
-
-        Actuals(
-          amountS,
-          (Rational(actuals.amountB) * r).bigintValue,
-          (Rational(actuals.amountFee) * r).bigintValue,
-          actuals.scale * r
-        )
-      }
-
-    }
-  }
 }
