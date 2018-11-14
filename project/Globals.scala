4a5852384655da0e415c3dcca7c1b091c991b02a
Daniel Wang <daniel@loopring.org>
2018-11-14 10:59:19 -0500

    protobuf

--- project/Globals.scala
+++ project/Globals.scala
@@ -1,6 +1,6 @@
 object Globals {
   lazy val name = "lightcore-core"
-  lazy val scalaVersion = "2.12.7"
+  lazy val scalaVersion = "2.12.6"
   lazy val projectGitHttpUrl = "https://github.com/loopring/lightcore-core"
   lazy val projectGitUrl = "git@github.com:Loopring/lightcone-core.git"
 }
