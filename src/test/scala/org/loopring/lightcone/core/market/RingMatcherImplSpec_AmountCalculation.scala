62c351f74aeafc01503170ae4cc87b4630b57e04
于红雨 <yuhongyu04@126.com>
2018-11-14 17:54:32 +0800

    mini change

--- src/test/scala/org/loopring/lightcone/core/market/RingMatcherImplSpec_AmountCalculation.scala
+++ src/test/scala/org/loopring/lightcone/core/market/RingMatcherImplSpec_AmountCalculation.scala
@@ -27,15 +27,21 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
     def isProfitable(ring: OrderRing, fiatValueThreshold: Double) = true
   }
   val matcher = new RingMatcherImpl()
-  var decimal = "000000000000000000000000000000"
-  decimal = decimal.substring(0, 18)
+  var decimal = "000000000000000000000000000000".substring(0, 18)
+  val (makerAmountS, makerAmountB, makerFee) = (
+    BigInt("100000000000" + decimal),
+    BigInt("10000000000" + decimal),
+    BigInt("10" + decimal)
+  )
+  val (takerAmountS, takerAmountB, takerFee) = (
+    BigInt("10000000000" + decimal),
+    BigInt("100000000000" + decimal),
+    BigInt("10" + decimal)
+  )
+  val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
+  val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
 
   "price just match" should "full fill if matchable is raw amount" in {
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("100000000000" + decimal), BigInt("10000000000" + decimal), BigInt("10" + decimal))
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
-    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
-    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
-
     val res = matcher.matchOrders(
       taker.copy(_matchable = Some(OrderState(amountS = takerAmountS, amountB = takerAmountB, amountFee = takerFee))),
       maker.copy(_matchable = Some(OrderState(amountS = makerAmountS, amountB = makerAmountB, amountFee = makerFee)))
@@ -56,10 +62,6 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
   }
 
   "price just match" should "part fill if both matchables are half raw amount" in {
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("100000000000" + decimal), BigInt("10000000000" + decimal), BigInt("10" + decimal))
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
-    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
-    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
     val res = matcher.matchOrders(
       taker.copy(_matchable = Some(OrderState(amountS = takerAmountS / 2, amountB = takerAmountB / 2, amountFee = takerFee / 2))),
       maker.copy(_matchable = Some(OrderState(amountS = makerAmountS / 2, amountB = makerAmountB / 2, amountFee = makerFee / 2)))
@@ -80,51 +82,36 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
   }
 
   "price3 just match" should "part fill if both matchables are a third of raw amount" in {
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("100000000000" + decimal), BigInt("10000000000" + decimal), BigInt("10" + decimal))
-    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
-    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
     //taker.amountB的计算通过amountS，因为精度问题，所以taker.amountB会在amountS的精度后为0
     val res = matcher.matchOrders(
-      taker.copy(_matchable = Some(OrderState(amountS = BigInt("333333333333333333333333333"), amountB = BigInt("3333333333333333333333333330"), amountFee = BigInt("333333333333333333")))),
-      maker.copy(_matchable = Some(OrderState(amountS = BigInt("3333333333333333333333333333"), amountB = BigInt("333333333333333333333333333"), amountFee = BigInt("333333333333333333"))))
+      taker.copy(_matchable = Some(OrderState(
+        amountS = BigInt("333333333333333333333333333"),
+        amountB = BigInt("3333333333333333333333333330"),
+        amountFee = BigInt("333333333333333333")
+      ))),
+      maker.copy(_matchable = Some(OrderState(
+        amountS = BigInt("3333333333333333333333333333"),
+        amountB = BigInt("333333333333333333333333333"),
+        amountFee = BigInt("333333333333333333")
+      )))
     )
     val expectRing = OrderRing(
       taker = ExpectedFill(
         order = taker.copy(_matchable = Some(OrderState())),
-        pending = OrderState(amountS = BigInt("333333333333333333333333333"), amountB = BigInt("3333333333333333333333333330"), amountFee = BigInt("333333333333333333")),
+        pending = OrderState(
+          amountS = BigInt("333333333333333333333333333"),
+          amountB = BigInt("3333333333333333333333333330"),
+          amountFee = BigInt("333333333333333333")
+        ),
         amountMargin = BigInt(0)
       ),
       maker = ExpectedFill(
         order = maker.copy(_matchable = Some(OrderState(amountS = 3, amountFee = 1))),
-        pending = OrderState(amountS = BigInt("3333333333333333333333333330"), amountB = BigInt("333333333333333333333333333"), amountFee = BigInt("333333333333333332")),
-        amountMargin = BigInt(0)
-      )
-    )
-    res.right.toOption should be(Some(expectRing))
-  }
-
-  "price2 just match" should "fill with different decimal and raw amount isn't divisible by matchable " in {
-    val otherDecimal = decimal.substring(0, 8)
-    info(decimal)
-    info(otherDecimal)
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("11" + otherDecimal), BigInt("110" + decimal), BigInt("10" + decimal))
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("110" + decimal), BigInt("11" + otherDecimal), BigInt("10" + decimal))
-    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
-    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
-    val res = matcher.matchOrders(
-      taker.copy(_matchable = Some(OrderState(amountS = BigInt("333333333"), amountB = BigInt("33333333300000000000"), amountFee = BigInt("3030303027272727272")))),
-      maker.copy(_matchable = Some(OrderState(amountS = BigInt("33333333333333333333"), amountB = BigInt("333333333"), amountFee = BigInt("3030303030303030303"))))
-    )
-    val expectRing = OrderRing(
-      taker = ExpectedFill(
-        order = taker.copy(_matchable = Some(OrderState())),
-        pending = OrderState(amountS = BigInt("333333333"), amountB = BigInt("33333333300000000000"), amountFee = BigInt("3030303027272727272")),
-        amountMargin = BigInt(0)
-      ),
-      maker = ExpectedFill(
-        order = maker.copy(_matchable = Some(OrderState(amountS = BigInt("33333333333"), amountFee = BigInt("3030303031")))),
-        pending = OrderState(amountS = BigInt("33333333300000000000"), amountB = BigInt("333333333"), amountFee = BigInt("3030303027272727272")),
+        pending = OrderState(
+          amountS = BigInt("3333333333333333333333333330"),
+          amountB = BigInt("333333333333333333333333333"),
+          amountFee = BigInt("333333333333333332")
+        ),
         amountMargin = BigInt(0)
       )
     )
@@ -132,10 +119,6 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
   }
 
   "price just match" should "part fill if one matchable is half of raw amount" in {
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("100000000000" + decimal), BigInt("10000000000" + decimal), BigInt("10" + decimal))
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
-    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
-    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
     val res = matcher.matchOrders(
       taker.copy(_matchable = Some(OrderState(amountS = takerAmountS / 2, amountB = takerAmountB / 2, amountFee = takerFee / 2))),
       maker.copy(_matchable = Some(OrderState(amountS = makerAmountS, amountB = makerAmountB, amountFee = makerFee)))
@@ -165,9 +148,60 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
     res.right.toOption should be(Some(expectRing))
   }
 
+  "price2 just match" should "fill with different decimal and raw amount isn't divisible by matchable " in {
+    val otherDecimal = decimal.substring(0, 8)
+    info(decimal)
+    info(otherDecimal)
+    val (takerAmountS, takerAmountB, takerFee) = (BigInt("11" + otherDecimal), BigInt("110" + decimal), BigInt("10" + decimal))
+    val (makerAmountS, makerAmountB, makerFee) = (BigInt("110" + decimal), BigInt("11" + otherDecimal), BigInt("10" + decimal))
+    val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
+    val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
+    val res = matcher.matchOrders(
+      taker.copy(_matchable = Some(OrderState(
+        amountS = BigInt("333333333"),
+        amountB = BigInt("33333333300000000000"),
+        amountFee = BigInt("3030303027272727272")
+      ))),
+      maker.copy(_matchable = Some(OrderState(
+        amountS = BigInt("33333333333333333333"),
+        amountB = BigInt("333333333"),
+        amountFee = BigInt("3030303030303030303")
+      )))
+    )
+    val expectRing = OrderRing(
+      taker = ExpectedFill(
+        order = taker.copy(_matchable = Some(OrderState())),
+        pending = OrderState(
+          amountS = BigInt("333333333"),
+          amountB = BigInt("33333333300000000000"),
+          amountFee = BigInt("3030303027272727272")
+        ),
+        amountMargin = BigInt(0)
+      ),
+      maker = ExpectedFill(
+        order = maker.copy(_matchable = Some(OrderState(amountS = BigInt("33333333333"), amountFee = BigInt("3030303031")))),
+        pending = OrderState(
+          amountS = BigInt("33333333300000000000"),
+          amountB = BigInt("333333333"),
+          amountFee = BigInt("3030303027272727272")
+        ),
+        amountMargin = BigInt(0)
+      )
+    )
+    res.right.toOption should be(Some(expectRing))
+  }
+
   "price1 with margin" should " fill if matchable is raw amount" in {
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("1500000000000" + decimal), BigInt("90000000000" + decimal), BigInt("90" + decimal))
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
+    val (makerAmountS, makerAmountB, makerFee) = (
+      BigInt("1500000000000" + decimal),
+      BigInt("90000000000" + decimal),
+      BigInt("90" + decimal)
+    )
+    val (takerAmountS, takerAmountB, takerFee) = (
+      BigInt("10000000000" + decimal),
+      BigInt("100000000000" + decimal),
+      BigInt("10" + decimal)
+    )
     val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
     val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
 
@@ -182,8 +216,16 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
         amountMargin = BigInt("2000000000" + decimal)
       ),
       maker = ExpectedFill(
-        order = maker.copy(_matchable = Some(OrderState(amountS = BigInt("700000000000" + decimal), amountB = BigInt("42000000000" + decimal), amountFee = BigInt("42" + decimal)))),
-        pending = OrderState(amountS = BigInt("50000000000" + decimal), amountB = BigInt("3000000000" + decimal), amountFee = BigInt("3" + decimal)),
+        order = maker.copy(_matchable = Some(OrderState(
+          amountS = BigInt("700000000000" + decimal),
+          amountB = BigInt("42000000000" + decimal),
+          amountFee = BigInt("42" + decimal)
+        ))),
+        pending = OrderState(
+          amountS = BigInt("50000000000" + decimal),
+          amountB = BigInt("3000000000" + decimal),
+          amountFee = BigInt("3" + decimal)
+        ),
         amountMargin = BigInt(0)
       )
     )
@@ -191,8 +233,16 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
   }
 
   "price with margin" should " fill if matchable is raw amount" in {
-    val (makerAmountS, makerAmountB, makerFee) = (BigInt("1500000000000" + decimal), BigInt("90000000000" + decimal), BigInt("90" + decimal))
-    val (takerAmountS, takerAmountB, takerFee) = (BigInt("10000000000" + decimal), BigInt("100000000000" + decimal), BigInt("10" + decimal))
+    val (makerAmountS, makerAmountB, makerFee) = (
+      BigInt("1500000000000" + decimal),
+      BigInt("90000000000" + decimal),
+      BigInt("90" + decimal)
+    )
+    val (takerAmountS, takerAmountB, takerFee) = (
+      BigInt("10000000000" + decimal),
+      BigInt("100000000000" + decimal),
+      BigInt("10" + decimal)
+    )
     val maker = sellDAI(makerAmountS, makerAmountB, makerFee)
     val taker = buyDAI(takerAmountS, takerAmountB, takerFee)
 
@@ -207,8 +257,16 @@ class RingMatcherImplSpec_AmountCalculation extends CommonSpec {
         amountMargin = BigInt("4000000000" + decimal)
       ),
       maker = ExpectedFill(
-        order = maker.copy(_matchable = Some(OrderState(amountS = BigInt("1400000000000" + decimal), amountB = BigInt("84000000000" + decimal), amountFee = BigInt("84" + decimal)))),
-        pending = OrderState(amountS = BigInt("100000000000" + decimal), amountB = BigInt("6000000000" + decimal), amountFee = BigInt("6" + decimal)),
+        order = maker.copy(_matchable = Some(OrderState(
+          amountS = BigInt("1400000000000" + decimal),
+          amountB = BigInt("84000000000" + decimal),
+          amountFee = BigInt("84" + decimal)
+        ))),
+        pending = OrderState(
+          amountS = BigInt("100000000000" + decimal),
+          amountB = BigInt("6000000000" + decimal),
+          amountFee = BigInt("6" + decimal)
+        ),
         amountMargin = BigInt(0)
       )
     )
